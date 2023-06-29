daeb396913e83a6454c3e7281693c95f85770d78
Kevin Wang <kevinwang_824@icloud.com>
2023-06-28 18:37:52 -0700

    Initial Commit

---
+++ Reservations/ReservationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReservationsApp.swift
+//  Reservations
+//
+//  Created by Kevin Wang on 6/28/23.
+//
+
+import SwiftUI
+
+@main
+struct ReservationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
